01e7e3c67447349ffa16111163400dbeef08cea9
Chen-Yu Pao <windperson@gmail.com>
2020-05-12 02:19:46 +0800

    init repo

---
+++ GranDen.GameLib.Bingo/MatchPrize.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GranDen.GameLib.Bingo
+{
+    public class MatchPrize
+    {
+        public MatchPrize(List<(int X, int Y)> MatchedPoints)
+        {
+            
+        }
+    }
+}

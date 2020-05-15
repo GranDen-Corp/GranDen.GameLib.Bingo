15f23a5649350abd83f654c69ad75ecf1c8b5687
Chen-Yu Pao <windperson@gmail.com>
2020-05-15 13:46:06 +0800

    Add documentations

--- src/GranDen.GameLib.Bingo/BingoPrizeClincher.cs
+++ src/GranDen.GameLib.Bingo/BingoPrizeClincher.cs
@@ -3,15 +3,27 @@ using System.Linq;
 
 namespace GranDen.GameLib.Bingo
 {
+    /// <summary>
+    /// 2D Bingo game achieved line detection engine
+    /// </summary>
     public class Bingo2dPrizeClincher
     {
         private readonly List<PrizeLine2D> _prizeLines;
 
+        /// <summary>
+        /// Create a 2D line bingo game line detection engine
+        /// </summary>
+        /// <param name="prizeLines"></param>
         public Bingo2dPrizeClincher(List<PrizeLine2D> prizeLines)
         {
             _prizeLines = prizeLines;
         }
 
+        /// <summary>
+        /// Decide currently achieved bingo line(s).
+        /// </summary>
+        /// <param name="inputs">The mark points in (X, Y, true/false) tuple format</param>
+        /// <returns></returns>
         public List<string> Decide(List<MarkPoint2D> inputs)
         {
             var matchedPrizes = new List<string>();

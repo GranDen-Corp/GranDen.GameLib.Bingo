f06396dd0ddb6bbeb6704347464b8ddc6e0ea363
Chen-Yu Pao <windperson@gmail.com>
2020-05-20 07:08:55 +0800

    fix typo in path

--- scripts/get_build_version.ps1
+++ scripts/get_build_version.ps1
@@ -3,5 +3,5 @@
 $version_text = `
 (Select-Xml -Path "./src/Directory.Build.props" -XPath "/Project/PropertyGroup/Version[contains(@Condition, `"`'`$(Version)`'==`'`'`")]//text()[1]").Node.Value;
 
-Add-Type -Path "./scripts/Nuget.Versioning.dll"
+Add-Type -Path "./scripts/NuGet.Versioning.dll"
 New-Object -TypeName "NuGet.Versioning.NuGetVersion" -ArgumentList "$version_text"

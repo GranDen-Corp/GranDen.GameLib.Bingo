7580187edd9b203c48191813ef4391718e95bf46
Chen-Yu Pao <windperson@gmail.com>
2020-05-20 14:07:47 +0800

    Fill some API documents

--- src/GranDen.GameLib.Bingo/Coordinates/IntPoint2D.cs
+++ src/GranDen.GameLib.Bingo/Coordinates/IntPoint2D.cs
@@ -5,15 +5,30 @@
     /// </summary>
     public class IntPoint2D
     {
+        /// <summary>
+        /// X coordinate (horizontal)
+        /// </summary>
         public int X { get; set; }
+        /// <summary>
+        /// Y coordinate (vertical)
+        /// </summary>
         public int Y { get; set; }
 
+        /// <summary>
+        /// class constructor by giving x, y coordinate values
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
         public IntPoint2D(int x, int y)
         {
             X = x;
             Y = y;
         }
 
+        /// <summary>
+        /// class constructor by giving (x, y) tuple
+        /// </summary>
+        /// <param name="pointTuple"></param>
         public IntPoint2D((int, int) pointTuple)
         {
             var (x, y) = pointTuple;
@@ -21,6 +36,11 @@
             Y = y;
         }
 
+        /// <summary>
+        /// Tuple deconstruct implementation
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
         public void Deconstruct(out int x, out int y)
         {
             x = X;

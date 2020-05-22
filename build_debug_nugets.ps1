535a2f6d34c58a724b589de28e066c986f98c4b7
Chen-Yu Pao <windperson@gmail.com>
2020-05-22 16:53:44 +0800

    Make default local build not generate NuGet.

--- build_debug_nugets.ps1
+++ build_debug_nugets.ps1
@@ -1,2 +1,2 @@
 #!/usr/bin/env pwsh
-Get-ChildItem src/*.csproj -Recurse | ForEach-Object { dotnet build -c Debug $_.FullName }
+Get-ChildItem src/*.csproj -Recurse | ForEach-Object { dotnet build -c Debug $_.FullName /p:GeneratePackageOnBuild=true }
